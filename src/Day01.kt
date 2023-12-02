c0419fdf7ad128b4b64d0907cd0aace45fc34bcc
Tomislav Strelar <tomislav.strelar@etranet.hr>
2023-12-02 12:19:16 +0100

    Day 1, part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,6 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>) = input.sumOf { line ->
+        "${line.first { it.isDigit() }}${line.last { it.isDigit() }}".toInt()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +9,10 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
+
