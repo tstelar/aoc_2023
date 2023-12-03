27501549382a8e5f6fc63a28d7bac080ef2b2e12
Tomislav Strelar <tomislav.strelar@etranet.hr>
2023-12-03 13:51:23 +0100

    Day 3, part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -8,32 +8,50 @@ fun main() {
         val symbols: Set<Int> = setOf()
     ) {
         fun sumParts(input: String) = symbols.flatMap {
-                val left = it - 1
-                val right = it + 1
-                val up = it - len
-                val down = it + len
-                val diagonalUpLeft = up - 1
-                val diagonalUpRight = up + 1
-                val diagonalDownLeft = down - 1
-                val diagonalDownRight = down + 1
+            val extracted = extract(it, input)
+            extracted
+        }.sum()
 
-                val extracted = mutableListOf<Int>()
-                if (contains(up)) {
-                    extracted.add(extractNumber(input, up).toInt())
-                } else {
-                    if (contains(diagonalUpLeft)) extracted.add(extractNumberLeft(input, diagonalUpLeft).toInt())
-                    if (contains(diagonalUpRight)) extracted.add(extractNumberRight(input, diagonalUpRight).toInt())
-                }
-                if (contains(down)) {
-                    extracted.add(extractNumber(input, down).toInt())
-                } else {
-                    if (contains(diagonalDownLeft)) extracted.add(extractNumberLeft(input, diagonalDownLeft).toInt())
-                    if (contains(diagonalDownRight)) extracted.add(extractNumberRight(input, diagonalDownRight).toInt())
-                }
-                if (contains(left)) { extracted.add(extractNumberLeft(input, left).toInt()) }
-                if (contains(right)) { extracted.add(extractNumberRight(input, right).toInt()) }
-                extracted
-            }.sum()
+        fun sumGearRatios(input: String) = symbols.sumOf {
+            val extracted = extract(it, input)
+            if (extracted.size == 2) {
+                extracted.fold(1, Int::times)
+            } else {
+                0
+            }
+        }
+
+        private fun extract(it: Int, input: String): MutableList<Int> {
+            val left = it - 1
+            val right = it + 1
+            val up = it - len
+            val down = it + len
+            val diagonalUpLeft = up - 1
+            val diagonalUpRight = up + 1
+            val diagonalDownLeft = down - 1
+            val diagonalDownRight = down + 1
+
+            val extracted = mutableListOf<Int>()
+            if (contains(up)) {
+                extracted.add(extractNumber(input, up).toInt())
+            } else {
+                if (contains(diagonalUpLeft)) extracted.add(extractNumberLeft(input, diagonalUpLeft).toInt())
+                if (contains(diagonalUpRight)) extracted.add(extractNumberRight(input, diagonalUpRight).toInt())
+            }
+            if (contains(down)) {
+                extracted.add(extractNumber(input, down).toInt())
+            } else {
+                if (contains(diagonalDownLeft)) extracted.add(extractNumberLeft(input, diagonalDownLeft).toInt())
+                if (contains(diagonalDownRight)) extracted.add(extractNumberRight(input, diagonalDownRight).toInt())
+            }
+            if (contains(left)) {
+                extracted.add(extractNumberLeft(input, left).toInt())
+            }
+            if (contains(right)) {
+                extracted.add(extractNumberRight(input, right).toInt())
+            }
+            return extracted
+        }
 
         private fun contains(idx: Int) = numbers.contains(idx)
 
@@ -70,17 +88,26 @@ fun main() {
         }
     }.sumParts(input.joinToString(""))
 
-    fun part2(input: List<String>) = input.sumOf { line ->
-        "${line.first { it.isDigit() }}${line.last { it.isDigit() }}".toInt()
-    }
+    fun part2(input: List<String>) = input.foldIndexed(Engine(input.first().length)) { lineIdx, acc, current ->
+        val globalIdx = lineIdx * acc.len
+        current.foldIndexed(acc) { idx, acc2, symbol ->
+            val globalLineIdx = globalIdx + idx
+            if (symbol.isDigit()) {
+                acc2.copy(numbers = acc2.numbers + globalLineIdx)
+            } else if (symbol == '*') {
+                acc2.copy(symbols = acc2.symbols + globalLineIdx)
+            } else {
+                acc2
+            }
+        }
+    }.sumGearRatios(input.joinToString(""))
 
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 4361)
-    val testInput2 = readInput("Day01_test2")
-//    check(part2(testInputnput2) == 281)
+    check(part2(testInput) == 467835)
 
     val input = readInput("Day03")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
 
